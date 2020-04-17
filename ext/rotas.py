0ad4ffdd7547f2a8dddcad8ded89a48f08127c3c
hldramos <helderoramos@live.com>
2020-04-16 22:11:59 -0300

    Tratamento de Visualização, Delete de Users

--- ext/rotas.py
+++ ext/rotas.py
@@ -1,5 +1,7 @@
 from flask import request, redirect, url_for, render_template
 
+from ext.database import db
+
 from models.users import User
 from models.products import Product
 
@@ -9,12 +11,29 @@ def init_app(app):
     def index():
         return render_template("index.html")
 
-    @app.route("/users")
+    @app.route("/users/")
     def users():
         usuario = User.query.all()
         return render_template("user.html", users=usuario)
 
-    @app.route("/products")
+    @app.route("/users/view/<int:id>")
+    def view_users(id):
+        usuario = User.query.get(id)
+        return render_template("view_user.html", users=usuario)
+
+    @app.route("/users/add")
+    def add_users():
+        usuario = User.query.all()
+        return render_template("user.html", users=usuario)
+
+    @app.route("/users/delete/<int:id>")
+    def delete_user(id=0):
+        usuario = User.query.filter_by(id=id).first()
+        db.session.delete(usuario)
+        db.session.commit()
+        return redirect("/")
+
+    @app.route("/products/")
     def products():
         produto = Product.query.all()
         return render_template("product.html", products=produto)
