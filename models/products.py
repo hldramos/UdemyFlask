dfb40c0c1cac670d3b3d8a992b51b3ef3e99df71
hldramos <helderoramos@live.com>
2020-04-20 20:57:02 -0300

    Refactory Models

--- models/products.py
+++
@@ -1,9 +0,0 @@
-from ext.database import db
-
-
-class Product(db.Model):
-    __tablename__ = "products"
-
-    id = db.Column(db.Integer(), primary_key=True)
-    descricao = db.Column(db.String(150), unique=True, nullable=False)
-    peso = db.Column(db.Integer())
